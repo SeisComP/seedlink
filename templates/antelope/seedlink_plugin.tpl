32b9587c7ef6d55c538a141c6862849a9a7fda68
Jan Becker <jabe@gempa.de>
2020-12-23 09:33:30 +0100

    [plugins] Fix Antelope configuration generation and add select parameter

--- templates/antelope/seedlink_plugin.tpl
+++ templates/antelope/seedlink_plugin.tpl
@@ -1,5 +1,5 @@
 * template: $template
-plugin $seedlink.source.id cmd="$seedlink.plugin_dir/orb_plugin -v -S $pkgroot/var/lib/seedlink/${seedlink.station.id}.pkt:100 -m '.*${seedlink.station.code}.*' -o $sources.orb.address:$sources.orb.port"
+plugin $seedlink.source.id cmd="$seedlink.plugin_dir/orb_plugin -v -S $pkgroot/var/lib/seedlink/${seedlink.station.id}.pkt:100 -m '.*${seedlink.station.code}.${sources.antelope.select}' -o $sources.antelope.address:$sources.antelope.port"
              timeout = 600
              start_retry = 60
              shutdown_wait = 10
--- templates/antelope/setup.py
+++ templates/antelope/setup.py
@@ -8,6 +8,18 @@ class SeedlinkPluginHandler:
   def __init__(self): pass
 
   def push(self, seedlink):
+    # Default host is localhost
+    try: seedlink.param('sources.antelope.address')
+    except: seedlink.setParam('sources.antelope.address', 'localhost')
+
+    # Default port is 39136
+    try: seedlink.param('sources.antelope.port')
+    except: seedlink.setParam('sources.antelope.port', 39136)
+
+    # Select defaults to '*'
+    try: seedlink.param('sources.antelope.select')
+    except: seedlink.setParam('sources.antelope.select', '*')
+
     # key is station (one instance per station)
     return seedlink.net + "." + seedlink.sta
 
