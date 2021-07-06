3e90d723be6884b320fbcb02fd8f4a8745f60de6
Andres Heinloo <andres@gfz-potsdam.de>
2021-07-06 14:00:01 +0200

    [serial] Add Vaisala ASCII protocol support

---
+++ templates/vaisala/setup.py
@@ -0,0 +1,26 @@
+import os
+
+'''
+Plugin handler for the MWS plugin.
+'''
+class SeedlinkPluginHandler:
+  # Create defaults
+  def __init__(self): pass
+
+  def push(self, seedlink):
+    # Check and set defaults
+    try: seedlink.param('sources.vaisala.comport')
+    except: seedlink.setParam('sources.vaisala.comport', '/dev/weatherstation')
+
+    try: seedlink.param('sources.vaisala.baudrate')
+    except: seedlink.setParam('sources.vaisala.baudrate', 19200)
+
+    try: seedlink.param('sources.vaisala.proc')
+    except: seedlink.setParam('sources.vaisala.proc', 'vaisala')
+
+    return seedlink.param('sources.vaisala.comport')
+
+
+  # Flush does nothing
+  def flush(self, seedlink):
+    pass
---
+++ templates/vaisala/streams_vaisala.tpl
@@ -0,0 +1,8 @@
+  <proc name="vaisala">
+    <tree>
+      <input name="KI" channel="KI" location="" rate="1/10"/>
+      <input name="II" channel="II" location="" rate="1/10"/>
+      <input name="DI" channel="DI" location="" rate="1/10"/>
+      <node stream="W"/>
+    </tree>
+  </proc>
